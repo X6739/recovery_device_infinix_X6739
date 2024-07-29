CK9n/recovery_tecno_CK9n

CK9n: Add TWRP version

BoardConfig.mk
@@ -186,3 +186,6 @@ TARGET_USES_LOGD := true
 # StatusBar
 TW_STATUS_ICONS_ALIGN := center
 TW_CUSTOM_CLOCK_POS := "300"
+
+# Version
+TW_DEVICE_VERSION := CK9n-DennisMurimi

X6739/recovery_device_infinix_X6739

BoardConfig.mk
@@ -186,3 +186,6 @@ TARGET_USES_LOGD := true
 # StatusBar
 TW_STATUS_ICONS_ALIGN := center
 TW_CUSTOM_CLOCK_POS := "300"
+
+# Version
+TW_DEVICE_VERSION := X6739-DennisMurimi